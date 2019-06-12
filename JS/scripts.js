74bc665f2a7437962f78f262401f223c21dfc912
Eric Lau <yasuyo369@gmail.com>
2019-06-11 22:37:43 -1000

    Pokemon
    Got pokemon script working

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,1 +1,15 @@
-alert('Hello World');
+let repository  = [
+  {name: "Venusaur ",  height:10, type:"Grass",},
+  {name: "Charizard ", height:14, type:"Fire"},
+  {name: "Blastoise ", height: 19, type:"Water"}
+];
+
+for(let a=0; a<repository.length; a++){
+  if(repository[a].height > 15){
+      document.write(repository[a].name +'(height:'+repository[a].height+')' +'- Wow, that’s big!');}
+  else{
+      document.write(repository[a].name +'(height:'+repository[a].height+')');
+      document.write("<br>");
+    }
+
+}
